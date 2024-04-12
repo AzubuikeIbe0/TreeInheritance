b6c86c6b8e7e292ccb2311fe0213420d3ba4e3c2
ai-dev-official <ai.codespro@gmail.com>
2024-04-12 13:02:10 +0100

    02

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    BinaryTree T1;
+    //BinaryTree T1;
 
    /* T1.InsertNode(1);
     T1.InsertNode(2);
@@ -25,23 +25,24 @@ int main()
     T1.InOrder();*/
 
     BinarySearchTree T2;
-
-    T2.InsertNode(1);
-    T2.InsertNode(2);
-    T2.InsertNode(3);
-    T2.InsertNode(4);
+    T2.InsertNode(25);
+    T2.InsertNode(10);
     T2.InsertNode(5);
+    T2.InsertNode(2);
+    T2.InsertNode(13);
+    T2.InsertNode(8);
+    
     T2.InsertNode(6);
     T2.InsertNode(7);
 
-    T2.PreOrder();
+    T2.InOrder();
     cout << "Printing in Pre Order" << endl;
    
-    T2.InOrder();
+    /*T2.InOrder();
     cout << "Printing in In Order" << endl;
     
     T2.PostOrder();
-    cout << "Printing in Post Order" << endl;
+    cout << "Printing in Post Order" << endl;*/
     T2.DeleteNode(2);
     cout << "Deleted Node 2" << endl;
     cout << endl;
@@ -55,8 +56,8 @@ int main()
     T2.InOrder();
     cout << "Printing in In Order After tree height" << endl;
 
-    T2.~BinarySearchTree();
-    T2.InOrder();
+    /*T2.~BinarySearchTree();
+    T2.InOrder();*/
 
     return 0;
 }
